6cbd7b743276029cb5f4ccbad12dff8cb79c88b0
Dhanashree <mhatredhanashree682@gmail.com>
2024-06-21 22:00:34 +0530

    add: real requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -13,11 +13,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import secrets
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 load_dotenv('.env')
@@ -29,7 +31,7 @@ load_dotenv('.env')
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_hex(32))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
