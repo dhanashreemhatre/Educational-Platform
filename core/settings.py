92d73e5e3031c8a74d6bca16cc12edc48a3977b9
Dhanashree <mhatredhanashree682@gmail.com>
2024-06-21 22:19:38 +0530

    done

--- core/settings.py
+++ core/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_hex(32))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost']
 
 SITE_ID=3
 
