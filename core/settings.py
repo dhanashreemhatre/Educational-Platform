f9f92a49857c4ba16a812b756b2598e6ac49aa95
Dhanashree <mhatredhanashree682@gmail.com>
2024-06-21 22:43:09 +0530

    all

--- core/settings.py
+++ core/settings.py
@@ -81,7 +81,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allauth.account.middleware.AccountMiddleware", 
 ]
 
 ROOT_URLCONF = "core.urls"
