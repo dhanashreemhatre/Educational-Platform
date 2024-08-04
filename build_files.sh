9b1b37b4cc390a008b8c5b46b9d1e082c7c2ee46
Dhanashree <mhatredhanashree682@gmail.com>
2024-08-04 18:37:16 +0530

    changed configuration

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,15 @@
+echo "BUILD START"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
+# install all deps in the venv
 pip install -r requirements.txt
-python ./manage.py collectstatic
+
+# collect static files using the Python interpreter from venv
+python manage.py collectstatic --noinput 
+
+echo "BUILD END"
